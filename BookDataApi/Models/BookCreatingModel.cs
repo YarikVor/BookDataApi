e38790603d545d7a61b514db932b3d6268fb296b
Yaroslav Vorobyov <official.vorobyov.yaroslav@gmail.com>
2023-02-25 21:59:19 +0200

    Added model classes

---
+++ BookDataApi/Models/BookCreatingModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookDataApi;
+
+public class BookCreatingModel
+{
+
+  public int id { get; set; }
+
+  [StringLength(128, MinimumLength = 1)]
+  [Required]
+  public string? title { get; set; }
+  [MaxLength(((65536 * 8 / 6 + 1 + 3) >> 2) << 2)]
+  [Required]
+
+  public string? cover { get; set; }
+  [MaxLength(65536 * 256)]
+  [Required]
+  public string? content { get; set; }
+  [MaxLength(128)]
+  [Required]
+  public string? genre { get; set; }
+  [MaxLength(64)]
+  [Required]
+  public string? author { get; set; }
+}
---
+++ BookDataApi/Models/ReviewCreateModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookDataApi
+{
+  public class ReviewCreatingModel
+  {
+    [MaxLength(512)]
+    [Required]
+    public string? message { get; set; }
+    [MaxLength(64)]
+    [Required]
+    public string? reviewer { get; set; }
+  }
+}
